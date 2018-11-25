acdb7ee7fc45fde5ba1c68ccd4601be9997573dc
Edouard Swiac <ed@quarantyne.com>
2018-11-25 15:09:09 -0800

    change logging level

--- src/main/java/com/quarantyne/classifiers/impl/IpRotationClassifier.java
+++ src/main/java/com/quarantyne/classifiers/impl/IpRotationClassifier.java
@@ -11,11 +11,13 @@ import com.quarantyne.lib.HttpRequestBody;
 import com.quarantyne.lib.RemoteIpAddresses;
 import java.time.Duration;
 import javax.annotation.Nullable;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Assumes that, in a 10 seconds window, two identical sets of headers are from the same agent
  * even if their IP is different
  */
+@Slf4j
 public class IpRotationClassifier implements HttpRequestClassifier {
   private final Cache<HashCode, RemoteIpAddresses> lastSeenCache;
 
@@ -32,6 +34,7 @@ public class IpRotationClassifier implements HttpRequestClassifier {
     HashCode headersHashcode = Fingerprinter.fromHeaders(httpRequest.getHeaders());
     RemoteIpAddresses seenIp = lastSeenCache.getIfPresent(headersHashcode);
     if (seenIp != null && !requestIp.equals(seenIp)) {
+      log.debug("request ip {} / seen ip {} / headers hashcode {}", requestIp, seenIp, headersHashcode);
       return Label.IP_ROTATION;
     }
 
